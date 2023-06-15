aac7dd7ddbd4437ea0a271aad8970df43495f208
Павел <patsey.pavel@gmail.com>
2023-06-15 21:06:37 +0300

    solve begin_04

---
+++ problem_book/03_begin/begin_04.exs
@@ -0,0 +1,6 @@
+# Begin4. Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
+# значения π использовать 3.14.
+
+d = String.to_integer(String.trim(IO.gets("Enter d\n")))
+l = :math.pi * d
+IO.puts("L = #{l}")
