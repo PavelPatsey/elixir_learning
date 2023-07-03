156ebb0231ec2cf66dcd4232b0d7cc9b72f5f2b4
Павел <patsey.pavel@gmail.com>
2023-07-03 20:08:04 +0300

    solve if 01

--- problem_book/06_if/if_01.exs
+++ problem_book/06_if/if_01.exs
@@ -1,12 +1,17 @@
 # If1. Дано целое число. Если оно является положительным, то прибавить к
 # нему 1; в противном случае не изменять его. Вывести полученное число.
 
-# n = IO.gets("Enter number\n")
-
 n =
   "Enter number\n"
   |> IO.gets()
   |> String.trim()
   |> String.to_integer()
 
-IO.puts(n)
+n =
+  if n > 0 do
+    n + 1
+  else
+    n
+  end
+
+IO.puts("n = #{n}")
