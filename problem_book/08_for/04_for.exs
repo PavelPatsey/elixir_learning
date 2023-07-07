d36eaecff9334fbda8245d841cb9f18bdfcd5137
Павел Пацей <patsey.pavel@gmail.com>
2023-07-07 21:35:32 +0300

    cosmetic changes mix format

--- problem_book/08_for/04_for.exs
+++ problem_book/08_for/04_for.exs
@@ -1,9 +1,9 @@
 # For4. Дано вещественное число — цена 1 кг конфет. Вывести стоимость 1,
 # 2, …, 10 кг конфет.
 
-n = IO.gets("Enter number\n")
+n =
+  IO.gets("Enter number\n")
   |> String.trim()
   |> String.to_integer()
 
-for k <- 1..10, do:
-  IO.puts(n*k)
+for k <- 1..10, do: IO.puts(n * k)
