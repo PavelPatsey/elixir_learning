063c7d9c91b81205f3d2d6bf231a1a7f027c8b4c
Павел <patsey.pavel@gmail.com>
2023-07-25 18:17:16 +0300

    cosmetic changes mix format

--- examples/recursion.exs
+++ examples/recursion.exs
@@ -2,11 +2,11 @@ defmodule TestElixir do
   def test_recursion(1) do
     1
   end
+
   def test_recursion(n) do
-    test_recursion(n-1)*n
+    test_recursion(n - 1) * n
   end
-  
 end
+
 IO.puts("Hello")
 IO.puts(TestElixir.test_recursion(10))
-
