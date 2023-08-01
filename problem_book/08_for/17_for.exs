206d6b871f40834ef071ed04d151b01106eab233
Павел Пацей <patsey.pavel@gmail.com>
2023-08-01 23:12:24 +0300

    solve 17_for

---
+++ problem_book/08_for/17_for.exs
@@ -0,0 +1,37 @@
+# For17. Дано вещественное число A и целое число N (> 0). Используя один цикл,
+# найти сумму
+# 1 + A + A2 + A3 + … + AN.
+
+# test
+# input
+# 1.2 1
+# output
+# 2.2
+
+# input
+# 1.2 3
+# output
+# 5.368
+
+a = 1.2
+n = 3
+
+[a_str, n_str] =
+  IO.gets("Enter float a and integer n\n")
+  |> String.trim()
+  |> String.split()
+
+a = elem(Float.parse(a_str), 0)
+n = elem(Integer.parse(n_str), 0)
+
+defmodule For17 do
+  def run(a, n) do
+    _run(a, n, 0)
+  end
+
+  defp _run(_, 0, acc), do: acc + 1
+
+  defp _run(a, n, acc), do: _run(a, n - 1, acc + a ** n)
+end
+
+IO.puts(For17.run(a, n))
