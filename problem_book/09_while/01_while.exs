59c67c379214b2833bbb768d021e06205d1f2028
Павел Пацей <patsey.pavel@gmail.com>
2023-08-05 13:05:36 +0300

    solve 01_while

---
+++ problem_book/09_while/01_while.exs
@@ -0,0 +1,32 @@
+# While1. Даны положительные числа A и B (A > B). На отрезке длины A разме-
+# щено максимально возможное количество отрезков длины B (без наложе-
+# ний). Не используя операции умножения и деления, найти длину незанятой
+# части отрезка A.
+
+# test
+# input
+# 3 1
+# output
+# 0
+
+# input
+# 4 3
+# output
+# 1
+
+[a, b] =
+  IO.gets("Enter a and b > 0\n")
+  |> String.trim()
+  |> String.split()
+  |> Enum.map(&String.to_integer/1)
+
+defmodule While01 do
+  def mod(a, b) do
+    cond do
+      a >= b -> mod(a - b, b)
+      a < b -> a
+    end
+  end
+end
+
+IO.puts(While01.mod(a, b))
