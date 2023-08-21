878d8a43dcbec77bfa0f9619b39a54ecd3079a5f
Павел <patsey.pavel@gmail.com>
2023-08-21 20:24:11 +0300

    correct 01_series

--- problem_book/10_series/01_series.exs
+++ problem_book/10_series/01_series.exs
@@ -4,14 +4,12 @@
 # input
 # 1 2 3 4 5 6 7 8 9 10
 # output
-# 0
+# 55
 
 list =
   IO.gets("Enter 10 float numbers\n")
   |> String.trim()
   |> String.split()
-  |> IO.inspect()
   |> Enum.map(&elem(Float.parse(&1), 0))
-  |> IO.inspect()
 
 IO.puts(Enum.sum(list))
